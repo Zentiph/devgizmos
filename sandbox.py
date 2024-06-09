a9b9af3bfda220948a03b0243142c45c581c6cc7
Gavin Borne <gavin.bor0925@gmail.com>
2024-06-09 09:25:52 -0400

    update gitignore

--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,3 @@
 # pylint: disable=all
 
 import devgizmos.decorators as decs
-from time import sleep
-
-
-@decs.timer()
-@decs.timeout(2)
-def process(i):
-    for _ in range(i):
-        pass
-
-
-process(1)
-process(1_000)
-process(1_000_000)
-process(1_000_000_000)
